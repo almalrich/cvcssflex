5769297c35844682ff3efd3b6c3f3bd74c6c78b0
almalrich <seb-bastos16@live.fr>
2018-11-17 08:12:46 +0100

    cv maison

---
+++ cvflex.js
@@ -0,0 +1,16 @@
+document.getElementById("btexp").addEventListener("click", function () {
+   if (document.getElementById("expro").style.display=="block"){
+       document.getElementById("expro").style.display="none";
+   } else {
+       document.getElementById("expro").style.display="block";
+   }
+});
+
+
+document.getElementById("btform").addEventListener("click", function () {
+    if (document.getElementById("form").style.display=="block"){
+        document.getElementById("form").style.display="none";
+    } else {
+        document.getElementById("form").style.display="block";
+    }
+});
